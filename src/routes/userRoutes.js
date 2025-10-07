2b49bb76e3e4840e1e82bc6843a27e413f0a3e51
Shivam Gaur <shivamgaur865029@gmail.com>
2025-10-07 13:16:41 +0530

    Refactor userRoutes to improve code organization and readability

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,24 +1,59 @@
 
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/userController.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccoutDetails,
+  updateAvatar,
+  updateCoverImage
+} from "../controllers/userController.js";
+import { upload } from "../middlewares/multer.middleware.js"
 import { jwtVerifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
+
 router.route("/register").post(
-    upload.fields([
-      {
-        name:"avatar",
-        maxCount:1
-      },
-      {
-        name:"coverImage",
-        maxCount:1
-      }
-    ]),
-    registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1
+    },
+    {
+      name: "coverImage",
+      maxCount: 1
+    }
+  ]),
+  registerUser
 )
-router.route("/login").post(loginUser)
-router.route("/logout").post(jwtVerifyJWT, logoutUser)
+router.route("/login")
+.post(loginUser)
+router.route("/logout")
+.post(jwtVerifyJWT, logoutUser)
+router.route("/refresh-token")
+.post(refreshAccessToken)
+router.route("/change-password")
+.post(jwtVerifyJWT, changeCurrentPassword)
+router.route("/current-user")
+.get(jwtVerifyJWT, getCurrentUser)
+router.route("/update-account")
+.patch(jwtVerifyJWT, updateAccoutDetails)
+router.route("/update-avatar")
+.patch(jwtVerifyJWT,
+  upload.single("avatar"), updateAvatar
+)
+router.route("/update-coverImage")
+.patch(jwtVerifyJWT,
+  upload.single("coverImage"), updateCoverImage
+)
+router.route("/user-channel-profile/:username")
+.post(jwtVerifyJWT, getUserChannelProfile)
+router.route("/watch-history")
+.post(jwtVerifyJWT, getWatchHistory)
 
 export default router;
